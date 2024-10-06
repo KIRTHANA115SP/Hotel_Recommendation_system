424d6524609e6c76e523089bf13c96ee48117dd4
KIRTHANA115SP <kirthana115sp@gmail.com>
2024-10-06 10:50:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+function searchHotels() {
+    alert("Redirecting to the Search Hotels page...");
+    window.location.href = "search.html"; // Redirect to search hotels page
+}
+
+function viewBookings() {
+    alert("Redirecting to My Bookings page...");
+    window.location.href = "bookings.html"; // Redirect to bookings page
+}
+
+function viewProfile() {
+    alert("Redirecting to My Profile page...");
+    window.location.href = "profile.html"; // Redirect to profile page
+}
